f6a3ad685179fd1edf36e9c316bda2131b52fea6
Dkui <44127730+Dkui97@users.noreply.github.com>
2024-02-28 23:45:45 +0800

    feat：MyBatis工具类SqlSessionUtil的封装

---
+++ mybatis-001-introduction/src/test/java/testInsertCar.java
@@ -0,0 +1,14 @@
+import com.powernode.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class testInsertCar {
+    @Test
+    public void testInsertCar() {
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        // 执行SQL
+        int count = sqlSession.insert("insertCar");
+        System.out.println("插入了几条记录:" + count);
+        sqlSession.close();
+    }
+}
